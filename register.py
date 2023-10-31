7f84cf241a4db27fbcffe084fca3766a52ba0b7b
SebaSkub <123407738+SebaSkub@users.noreply.github.com>
2023-10-31 17:25:37 -0400

    Update register.py

--- register.py
+++ register.py
@@ -4,7 +4,7 @@ import hashlib
 from flask import Flask, request
 
 # RabbitMQ connection parameters
-rabbitmq_host = 'sar490'  # Update with your RabbitMQ server host
+rabbitmq_host = 'sars490'  # Update with your RabbitMQ server host
 rabbitmq_port = 5672
 rabbitmq_user = 'it490'
 rabbitmq_password = 'it490'
