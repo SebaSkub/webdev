6249dbda3ffcb05f4d849fb076c3edcd6cbbe169
SebaSkub <123407738+SebaSkub@users.noreply.github.com>
2023-10-30 20:06:03 -0400

    Update register.py

--- register.py
+++ register.py
@@ -1,60 +1,64 @@
-# Import necessary libraries
-from flask import Flask, request, render_template
 import pika
-from pika.credentials import PlainCredentials as PikaCredentials
-import json
+import cgi
+import sys
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-app = Flask(__name)
+# RabbitMQ connection parameters
+RABBITMQ_HOST = '10.198.120.114'  # Update with your RabbitMQ server host
+RABBITMQ_PORT = 5672
+RABBITMQ_USER = 'it490'
+RABBITMQ_PASSWORD = 'it490'
+RABBITMQ_QUEUE = 'userRegister_FTOB'
 
-# Configure RabbitMQ connection parameters
-rabbitmq_host = '10.198.120.114'  # Update with your RabbitMQ server host
-rabbitmq_port = 5672
-rabbitmq_user = 'it490'
-rabbitmq_password = 'it490'
-rabbitmq_queue = 'userRegister_FTOB'
+# Define a class to handle HTTP requests
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        form = cgi.FieldStorage(
+            fp=self.rfile,
+            headers=self.headers,
+            environ={'REQUEST_METHOD': 'POST'}
+        )
+        # Extract form data
+        email = form.getvalue('email')
+        first = form.getvalue('first')
+        last = form.getvalue('last')
+        DOB = form.getvalue('DOB')
+        age = form.getvalue('age')
+        lolID = form.getvalue('lolID')
+        steamLink = form.getvalue('steamLink')
+        secQuest1 = form.getvalue('secQuest1')
+        secQuest2 = form.getvalue('secQuest2')
+        user = form.getvalue('user')
+        passw = form.getvalue('pass')
 
-# Define a function to send data to RabbitMQ
-def send_to_rabbitmq(data):
-    credentials = PikaCredentials(rabbitmq_user, rabbitmq_password)
-    parameters = pika.ConnectionParameters(host=rabbitmq_host, port=rabbitmq_port, credentials=credentials)
-    connection = pika.BlockingConnection(parameters)
-    channel = connection.channel()
-    channel.queue_declare(queue=rabbitmq_queue)
+        # Format the data as a message
+        message = f"Email: {email}, First Name: {first}, Last Name: {last}, DOB: {DOB}, Age: {age}, LOL ID: {lolID}, Steam Link: {steamLink}, Security Question #1: {secQuest1}, Security Question #2: {secQuest2}, Username: {user}, Password: {passw}"
 
-    # Convert data to JSON
-    message = json.dumps(data)
+        # Publish the message to RabbitMQ
+        credentials = pika.PlainCredentials(RABBITMQ_USER, RABBITMQ_PASSWORD)
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(
+                host=RABBITMQ_HOST,
+                port=RABBITMQ_PORT,
+                credentials=credentials
+            )
+        )
+        channel = connection.channel()
+        channel.queue_declare(queue=RABBITMQ_QUEUE)
+        channel.basic_publish(exchange='', routing_key=RABBITMQ_QUEUE, body=message)
+        connection.close()
 
-    # Send the data to the RabbitMQ queue
-    channel.basic_publish(exchange='', routing_key=rabbitmq_queue, body=message)
-
-    connection.close()
-
-# Define a route to handle the form submission
-@app.route('/it490/register', methods=['POST'])
-def register():
-    if request.method == 'POST':
-        # Get form data
-        data = {
-            'type': 'register',
-            'email': request.form.get('email'),
-            'first_name': request.form.get('first'),
-            'last_name': request.form.get('last'),
-            'DOB': request.form.get('DOB'),
-            'age': request.form.get('age'),
-            'lol_id': request.form.get('lolID'),
-            'steam_link': request.form.get('steamLink'),
-            'sec_question_1': request.form.get('secQuest1'),
-            'sec_question_2': request.form.get('secQuest2'),
-            'username': request.form.get('user'),
-            'password': request.form.get('pass')
-        }
-
-        try:
-            # Send data to RabbitMQ
-            send_to_rabbitmq(data)
-            return "User registration submitted successfully."
-        except Exception as e:
-            return f"Error: {str(e)}"
+        # Send a response to the browser
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b'Registration successful!')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=7007)
+    try:
+        server = HTTPServer(('', 80), RequestHandler)
+        print('Started HTTP server')
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print('^C received, shutting down the server')
+        server.socket.close()
