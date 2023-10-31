9ae1f4936d96906f80f4eda8bde4fecb24a6e37f
SebaSkub <123407738+SebaSkub@users.noreply.github.com>
2023-10-31 16:32:17 -0400

    Update register.py

--- register.py
+++ register.py
@@ -4,7 +4,7 @@ from flask import Flask, request
 
 
 # RabbitMQ connection parameters
-RABBITMQ_HOST = '10.198.120.114'  # Update with your RabbitMQ server host
+RABBITMQ_HOST = 'it490'  # Update with your RabbitMQ server host
 RABBITMQ_PORT = 5672
 RABBITMQ_USER = 'it490'
 RABBITMQ_PASSWORD = 'it490'
@@ -24,20 +24,20 @@ channel = connection.channel()
 # Declare a queue
 channel.queue_declare(queue=RABBITMQ_QUEUE)
 
-def send_to_rabbitmq(data):
-    try:
+#def send_to_rabbitmq(data):
+    #try:
         # Publish the user registration data to the RabbitMQ queue
-        channel.basic_publish(
-            exchange='',
-            routing_key=RABBITMQ_QUEUE,
-            body=json.dumps(data),
-            properties=pika.BasicProperties(
-                delivery_mode=2,  # Make the message persistent
-            )
-        )
-        print(" [x] Sent user data to RabbitMQ")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+       # channel.basic_publish(
+           # exchange='',
+            #routing_key=RABBITMQ_QUEUE,
+            #body=json.dumps(data),
+            #properties=pika.BasicProperties(
+               # delivery_mode=2,  # Make the message persistent
+           # )
+        #
+        #print(" [x] Sent user data to RabbitMQ")
+   # except Exception as e:
+       # print(f"An error occurred: {e}")
 @app.route('/register', methods=['POST'])
 def register():
     user_data = {
@@ -58,15 +58,15 @@ def register():
     return "Registration successful"  # You can handle success and response as needed
 
 # Example endpoint to handle the user registration
-@app.route('/register', methods=['POST'])
-def register():
-    user_data = request.json  # Assuming the data is received as JSON
+#@app.route('/register', methods=['POST'])
+#def register():
+    #user_data = request.json  # Assuming the data is received as JSON
     # You can add data validation and additional processing here
 
     # Send user data to RabbitMQ
-    send_to_rabbitmq(user_data)
+    #send_to_rabbitmq(user_data)
 
-    return "Registration successful"
+    #return "Registration successful"
 
 if __name__ == '__main__':
     app.run()
