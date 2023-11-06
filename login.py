f2c4e68063cba575fbe6285ab175fb89b8efded0
SebaSkub <123407738+SebaSkub@users.noreply.github.com>
2023-11-06 17:57:57 -0500

    Update login.py

--- login.py
+++ login.py
@@ -45,11 +45,11 @@ def login():
 if __name__ == '__main__':
 from flask import Flask
 
-app = Flask(__name)
+app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=7007)
+    app.run(host=it490skubisz, port=7007)
