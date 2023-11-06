ea7138fe1b52f3568d8de1b25a5c21ed036c03db
SebaSkub <123407738+SebaSkub@users.noreply.github.com>
2023-11-06 17:11:57 -0500

    Update login.py

--- login.py
+++ login.py
@@ -1,26 +1,3 @@
-from flask import Flask, request, jsonify
-import pika
-import json
-
-app = Flask(__name)
-# RabbitMQ configurations
-rabbitmq_host = 'sars490'  # Replace with your RabbitMQ server host
-rabbitmq_port = 5672
-rabbitmq_user = 'it490'
-rabbitmq_password = 'it490'
-rabbitmq_queue = 'userLogin_FTOB'  # Replace with the name of your login queue
-
-# Initialize a connection to RabbitMQ
-credentials = pika.PlainCredentials(username=rabbitmq_user, password=rabbitmq_password)
-parameters = pika.ConnectionParameters(host=rabbitmq_host, port=rabbitmq_port, credentials=credentials)
-connection = pika.BlockingConnection(parameters)
-channel = connection.channel()
-channel.queue_declare(queue=rabbitmq_queue)
-
-# Define a function to send data to RabbitMQ as plaintext
-def send_to_rabbitmq(data):
-    channel.basic_publish(exchange='', routing_key=rabbitmq_queue, body=data)
-
 @app.route('/login', methods=['POST'])
 def login():
     if request.method == 'POST':
@@ -33,9 +10,11 @@ def login():
 
             if username == 'example_username' and password == 'example_password':
                 # Assuming valid login, send data to RabbitMQ
-                login_data = f'Username: {username}, Password: {password}'
+                login_data = f"{username}, {password}"
                 send_to_rabbitmq(login_data)
-                return 'Login successful', 200
+                # Redirect to the landing page after successful login
+                return redirect('/it490/landing.html')
+
             else:
                 return 'Invalid username or password', 401
 
