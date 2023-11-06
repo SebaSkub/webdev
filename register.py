e0eed6e750635db0e19369a0507dd499e5e1b712
SebaSkub <123407738+SebaSkub@users.noreply.github.com>
2023-11-06 17:49:35 -0500

    Update register.py

--- register.py
+++ register.py
@@ -47,4 +47,13 @@ def register():
         return redirect('/it490/login.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    from flask import Flask
+
+app = Flask(__name)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=7007)
