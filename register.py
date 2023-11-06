cf5ef8b986bcafdff49e75d3b0e6f520239a3fc2
SebaSkub <123407738+SebaSkub@users.noreply.github.com>
2023-11-06 16:42:21 -0500

    Update register.py

--- register.py
+++ register.py
@@ -1,6 +1,5 @@
 import pika
-from flask import Flask, request, render_template
-import json
+from flask import Flask, request
 
 # RabbitMQ connection parameters
 rabbitmq_host = 'sars490'  # Update with your RabbitMQ server host
@@ -10,14 +9,15 @@ rabbitmq_password = 'it490'
 rabbitmq_queue = 'userRegister_FTOB'
 
 app = Flask(__name)
-    # Configure the RabbitMQ connection
-    credentials = pika.PlainCredentials(username=rabbitmq_user, password=rabbitmq_password)
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_host, port=rabbitmq_port, credentials=credentials))
-    channel = connection.channel()
-    # Declare a queue for user registration data
-    channel.queue_declare(queue=rabbitmq_queue, durable=True)
 
-    
+# Configure the RabbitMQ connection
+credentials = pika.PlainCredentials(username=rabbitmq_user, password=rabbitmq_password)
+connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_host, port=rabbitmq_port, credentials=credentials))
+channel = connection.channel()
+
+# Declare a queue for user registration data
+channel.queue_declare(queue=rabbitmq_queue, durable=True)
+
 @app.route('/register', methods=['POST'])
 def register():
     if request.method == 'POST':
@@ -33,8 +33,8 @@ def register():
         username = request.form['username']
         password = request.form['password']
 
-        # Prepare the data as a plaintext message
-        registration_data = f"First Name: {first_name}\nLast Name: {last_name}\nDate of Birth: {dob}\nAge: {age}\nLeague of Legends ID: {lol_id}\nSteam Link: {steam_link}\nSecurity Question #1: {security_question1}\nSecurity Question #2: {security_question2}\nUsername: {username}\nPassword: {password}"
+        # Prepare the data as a comma-separated string
+        registration_data = f"{first_name},{last_name},{dob},{age},{lol_id},{steam_link},{security_question1},{security_question2},{username},{password}"
 
         # Publish the data to RabbitMQ
         channel.basic_publish(exchange='', routing_key=rabbitmq_queue, body=registration_data)
