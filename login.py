1d3436cc85b5d4603238ebd229bbcd917ec8e599
SebaSkub <123407738+SebaSkub@users.noreply.github.com>
2023-11-07 18:39:14 -0500

    Update login.py

--- login.py
+++ login.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, jsonify
 import pika
-import json
 
-app = Flask(__name__)
+app = Flask(__name)
+
 # RabbitMQ configurations
 rabbitmq_host = 'sars490'  # Replace with your RabbitMQ server host
 rabbitmq_port = 5672
@@ -14,33 +14,29 @@ rabbitmq_queue = 'userLogin_FTOB'  # Replace with the name of your login queue
 credentials = pika.PlainCredentials(username=rabbitmq_user, password=rabbitmq_password)
 connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_host, port=rabbitmq_port, credentials=credentials))
 channel = connection.channel()
-channel.queue_declare(queue=rabbitmq_queue, durable = True)
+channel.queue_declare(queue=rabbitmq_queue, durable=True)
 
 # Define a function to send data to RabbitMQ as plaintext
 def send_to_rabbitmq(data):
     channel.basic_publish(exchange='', routing_key=rabbitmq_queue, body=data)
 
+# Your login route
 @app.route('/login', methods=['POST'])
 def login():
-    if request.method == 'POST':
-        try:
-            username = request.form.get('user')
-            password = request.form.get('pass')
+    try:
+        if request.method == 'POST':
+            username = request.form.get('username')
+            password = request.form.get('password')
+                # Assuming valid login, send data to RabbitMQ in the desired format
+            login_data = f'{username},{password}'
+            send_to_rabbitmq(login_data)
 
-            # Validate user's credentials (you can implement your own validation logic here)
-            # For example, check if the username and password are correct
-
-            if username == 'user' and password == 'pass':
-                # Assuming valid login, send data to RabbitMQ
-                login_data = f"{username}, {password}"
-                send_to_rabbitmq(login_data)
-                return redirect('/it490/landing.html')
-
+                return 'Login successful', 200
             else:
                 return 'Invalid username or password', 401
 
-        except Exception as e:
-            return 'Login failed', 400
+    except Exception as e:
+        return 'Login failed', 400
 
 if __name__ == '__main__':
     app.run(host='10.198.120.126', port=7007)
