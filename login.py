789f8cf85621c74aea3b61107d0c23b17f6ec78e
SebaSkub <123407738+SebaSkub@users.noreply.github.com>
2023-11-07 18:54:07 -0500

    Update login.py

--- login.py
+++ login.py
@@ -10,15 +10,24 @@ rabbitmq_user = 'it490'
 rabbitmq_password = 'it490'
 rabbitmq_queue = 'userLogin_FTOB'  # Replace with the name of your login queue
 
-# Initialize a connection to RabbitMQ
-credentials = pika.PlainCredentials(username=rabbitmq_user, password=rabbitmq_password)
-connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_host, port=rabbitmq_port, credentials=credentials))
-channel = connection.channel()
-channel.queue_declare(queue=rabbitmq_queue, durable=True)
-
 # Define a function to send data to RabbitMQ as plaintext
 def send_to_rabbitmq(data):
-    channel.basic_publish(exchange='', routing_key=rabbitmq_queue, body=data)
+    try:
+        # Initialize a connection to RabbitMQ
+        credentials = pika.PlainCredentials(username=rabbitmq_user, password=rabbitmq_password)
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_host, port=rabbitmq_port, credentials=credentials))
+        channel = connection.channel()
+        channel.queue_declare(queue=rabbitmq_queue, durable=True)
+
+        # Send the data to RabbitMQ in the desired format
+        channel.basic_publish(exchange='', routing_key=rabbitmq_queue, body=data)
+
+        # Close the connection
+        connection.close()
+
+    except Exception as e:
+        # Handle exceptions if the connection or message sending fails
+        print(f"Failed to send data to RabbitMQ: {str(e)}")
 
 # Your login route
 @app.route('/login', methods=['POST'])
@@ -29,12 +38,13 @@ def login():
             password = request.form.get('password')
 
             # Assuming valid login, send data to RabbitMQ in the desired format
-            login_data = f'Username: {username}, Password: {password}'
+            login_data = f'{username},{password}'
             send_to_rabbitmq(login_data)
 
             return 'Login successful', 200
 
         except Exception as e:
             return 'Login failed', 400
+
 if __name__ == '__main__':
     app.run(host='10.198.120.126', port=7007)
