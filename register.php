77df049397d110a487220a551bf2c8459c2dcb28
SebaSkub <123407738+SebaSkub@users.noreply.github.com>
2023-11-07 19:59:21 -0500

    Update register.php

--- register.php
+++ register.php
@@ -1,1 +1,57 @@
+<?php
+require_once 'vendor/autoload.php'; // Include the amqplib library
 
+$rabbitmq_host = 'sars490'; // Update with your RabbitMQ server host
+$rabbitmq_port = 5672;
+$rabbitmq_user = 'it490';
+$rabbitmq_password = 'it490';
+$rabbitmq_queue = 'userRegister_FTOB';
+
+function send_to_rabbitmq($data) {
+    global $rabbitmq_host, $rabbitmq_port, $rabbitmq_user, $rabbitmq_password, $rabbitmq_queue;
+
+    try {
+        // Initialize a connection to RabbitMQ
+        $credentials = new PhpAmqpLib\Wire\AMQPTable(['login' => $rabbitmq_user, 'password' => $rabbitmq_password]);
+        $connection = new PhpAmqpLib\Connection\AMQPStreamConnection($rabbitmq_host, $rabbitmq_port, $rabbitmq_user, $rabbitmq_password);
+        $channel = $connection->channel();
+        $channel->queue_declare($rabbitmq_queue, false, true, false, false);
+
+        // Send the data to RabbitMQ in the desired format
+        $message = new PhpAmqpLib\Message\AMQPMessage($data);
+        $channel->basic_publish($message, '', $rabbitmq_queue);
+
+        // Close the connection
+        $channel->close();
+        $connection->close();
+    } catch (Exception $e) {
+        // Handle exceptions if the connection or message sending fails
+        echo "Failed to send data to RabbitMQ: " . $e->getMessage();
+    }
+}
+
+// Handle the form submission
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Collect form data
+    $first_name = $_POST['firstName'];
+    $last_name = $_POST['lastName'];
+    $dob = $_POST['dob'];
+    $age = $_POST['age'];
+    $lol_id = $_POST['lolId'];
+    $steam_link = $_POST['steamLink'];
+    $security_question1 = $_POST['securityQuestion1'];
+    $security_question2 = $_POST['securityQuestion2'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Prepare the data as a comma-separated string
+    $registration_data = "$first_name,$last_name,$dob,$age,$lol_id,$steam_link,$security_question1,$security_question2,$username,$password";
+
+    // Use the send_to_rabbitmq function to publish the data to RabbitMQ
+    send_to_rabbitmq($registration_data);
+
+    // Redirect to the login page after successful registration
+    header("Location: /it490/login.html");
+    exit;
+}
+?>
