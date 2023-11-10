9b1df90fddf95a732c3e72d246d93704b807593f
SebaSkub <123407738+SebaSkub@users.noreply.github.com>
2023-11-10 13:08:40 -0500

    Update login.php

--- login.php
+++ login.php
@@ -2,18 +2,14 @@
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
-require_once 'vendor/autoload.php'; // Include the Composer autoloader
-
+require_once __DIR__ 'vendor/autoload.php';
+// Include the Composer autoloader
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 $rabbitmq_host = 'it490mjt'; // Update with your RabbitMQ server host
 $rabbitmq_port = 5672;
 $rabbitmq_user = 'it490';
 $rabbitmq_password = 'it490';
 $rabbitmq_queue = 'test'; // Replace with the name of your login queue
-
-function send_to_rabbitmq($data) {
-    global $rabbitmq_host, $rabbitmq_port, $rabbitmq_user, $rabbitmq_password, $rabbitmq_queue;
-
-    try {
         // Initialize a connection to RabbitMQ
         $connection = new AMQPStreamConnection($rabbitmq_host, $rabbitmq_port, $rabbitmq_user, $rabbitmq_password);
         $channel = $connection->channel();
@@ -25,22 +21,14 @@ function send_to_rabbitmq($data) {
 
         // Close the connection
         $channel->close();
-        $connection->close();
-    } catch (Exception $e) {
-        // Handle exceptions if the connection or message sending fails
-        echo "Failed to send data to RabbitMQ: " . $e->getMessage();
-    }
-}
+        $connection->close()
 
-// Your login route
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    try {
         $username = $_POST['username'];
         $password = $_POST['password'];
 
         // Assuming valid login, send data to RabbitMQ in the desired format
         $login_data = "$username,$password";
-        send_to_rabbitmq($login_data);
+        echo($login_data);
         header("Location: /it490/landing.html");
         exit;
     } catch (Exception $e) {
