00847a5541edf186fae67b85c6f285b7e9c9358c
SebaSkub <123407738+SebaSkub@users.noreply.github.com>
2023-11-10 15:39:46 -0500

    Update login.php

--- login.php
+++ login.php
@@ -2,37 +2,37 @@
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
-require_once __DIR__ '/vendor/autoload.php';
-// Include the Composer autoloader
+require_once __DIR__ . '/vendor/autoload.php'; // Include the Composer autoloader
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-$rabbitmq_host = 'it490mjt'; // Update with your RabbitMQ server host
-$rabbitmq_port = 5672;
-$rabbitmq_user = 'it490';
-$rabbitmq_password = 'it490';
-$rabbitmq_queue = 'test';
-$username = $_POST['username'];
-$password = $_POST['password'];
-$message = "$username,$password";
-        // Replace with the name of your login queue
-        // Initialize a connection to RabbitMQ
-        $connection = new AMQPStreamConnection($rabbitmq_host, $rabbitmq_port, $rabbitmq_user, $rabbitmq_password);
-        $channel = $connection->channel();
-        $channel->queue_declare($rabbitmq_queue, false, true, false, false);
+    $rabbitmq_host = 'it490mjt'; // Update with your RabbitMQ server host
+    $rabbitmq_port = 5672;
+    $rabbitmq_user = 'it490';
+    $rabbitmq_password = 'it490';
+    $rabbitmq_queue = 'tst';
 
-        // Send the data to RabbitMQ in the desired format
-        $message = new AMQPMessage($data);
-        $channel->basic_publish($message, '', $rabbitmq_queue);
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-        // Close the connection
-        $channel->close();
-        $connection->close()
+    // Prepare the data as a comma-separated string
+    $data = "$username,$password";
 
-        
+    // Initialize a connection to RabbitMQ
+    $connection = new AMQPStreamConnection($rabbitmq_host, $rabbitmq_port, $rabbitmq_user, $rabbitmq_password);
+    $channel = $connection->channel();
+    $channel->queue_declare($rabbitmq_queue, false, true, false, false);
 
-        // Assuming valid login, send data to RabbitMQ in the desired format
-        echo($message);
-        header("Location: /landing.html");
-        exit;
-    
+    // Send the data to RabbitMQ in the desired format
+    $message = new AMQPMessage($data);
+    $channel->basic_publish($message, '', $rabbitmq_queue);
+
+    // Close the connection
+    $channel->close();
+    $connection->close();
+
+    // Assuming valid login, send data to RabbitMQ in the desired format
+    echo $message;
+    header("Location: /it490/landing.html");
+    exit;
 }
 ?>
